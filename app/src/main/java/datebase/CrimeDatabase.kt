ea72206edda7bda703318426f7f561a02320047e
Frisel91 <goryach.1991@mail.ru>
2022-04-27 21:48:22 +0300

    доработки Добавление базы данных

---
+++ app/src/main/java/datebase/CrimeDatabase.kt
@@ -0,0 +1,11 @@
+package datebase
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.example.mycryintent.Crime
+
+@Database(entities = [Crime::class], version = 1)
+@TypeConverters(CrimeTypeConverters::class)
+abstract class CrimeDatabase: RoomDatabase() {
+}
